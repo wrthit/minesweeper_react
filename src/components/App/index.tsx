a8621533edc11ab1c66473e7b8465c761502e04d
Joseph Hicks <jthicks@gmail.com>
2021-01-06 10:04:56 -0600

    styled header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.scss";
+import NumberDisplay from "../NumberDisplay";
 
 const App: React.FC = () => {
-  return <div className="App">Hello from Appl</div>;
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">
+          <span role="img" aria-label="face">
+            🙂
+          </span>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">Body</div>
+    </div>
+  );
 };
 
 export default App;
